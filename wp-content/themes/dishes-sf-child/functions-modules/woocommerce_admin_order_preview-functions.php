b97a03c38abd443c558eef543313a61d88c98224
knastnt <456kot@mail.ru>
2019-01-12 21:14:43 +1000

    получилось изменить данные в превью!!!!

--- wp-content/themes/dishes-sf-child/functions-modules/woocommerce_admin_order_preview-functions.php
+++ wp-content/themes/dishes-sf-child/functions-modules/woocommerce_admin_order_preview-functions.php
@@ -5,6 +5,11 @@
 //https://wp-kama.ru/function/WC_Admin_List_Table_Orders::order_preview_template
 //////////////////////////////////
 
+
+
+
+
+
 add_action( 'woocommerce_admin_order_preview_start', 'action_function_name_7949' );
 function action_function_name_7949(){
 	echo "!!!!!!!!!!";
@@ -14,12 +19,14 @@ function action_function_name_7949(){
 // Add custom order meta data to make it accessible in Order preview template
 add_filter( 'woocommerce_admin_order_preview_get_order_details', 'admin_order_preview_add_custom_meta_data', 10, 2 );
 function admin_order_preview_add_custom_meta_data( $data, $order ) {
-	//$data = print_r($data, 1) . PHP_EOL;
-    //file_put_contents($_SERVER['DOCUMENT_ROOT'].'/dump.txt', $data, FILE_APPEND);
+	/*$data = print_r($data, 1) . PHP_EOL;
+    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/dump.txt', $data, FILE_APPEND);*/
 	
     // Replace '_custom_meta_key' by the correct postmeta key
     if( $custom_value = $order->get_meta('_custom_meta_key') )
         $data['custom_key'] = $custom_value; // <= Store the value in the data array.
+       
+	$data['formatted_billing_address'] = 1111111; // <= Store the value in the data array.
 
     return $data;
 }
@@ -30,3 +37,4 @@ function custom_display_order_data_in_admin(){
     // Call the stored value and display it
     echo '<div>Value: {{data.custom_key}}</div><br>';
 }
+
