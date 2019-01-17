1be3f54f4a8fdf82b438b0a701c9666aaa6af36b
knastnt <456kot@mail.ru>
2019-01-17 10:50:24 +1000

    подправил min

--- wp-content/themes/dishes-sf-child/woocommerce/global/quantity-input.php
+++ wp-content/themes/dishes-sf-child/woocommerce/global/quantity-input.php
@@ -39,7 +39,7 @@ if ( $max_value && $min_value === $max_value ) {
 			id="<?php echo esc_attr( $input_id ); ?>"
 			class="input-text qty text quantity-num"
 			step="<?php echo esc_attr( $step ); ?>"
-			min="1"
+			min="<?php if($min_value == 0) $min_value++; echo esc_attr( $min_value ); ?>"
 			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
 			name="<?php echo esc_attr( $input_name ); ?>"
 			value="<?php echo esc_attr( $input_value ); ?>"
