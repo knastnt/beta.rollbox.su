80ba3c598cef00edb50a69a6a18e1d37479d38c9
knastnt <456kot@mail.ru>
2019-01-26 21:09:59 +1000

    так-то лучше

--- wp-content/themes/dishes-sf-child/functions.php
+++ wp-content/themes/dishes-sf-child/functions.php
@@ -4,6 +4,16 @@
 define("SHOW_MSG_IF_NOT_LOGGINED", false);
 define("DEBUG_MODE", false);
 
+
+//подмена core js в woocommerce
+add_action( 'wp_enqueue_scripts', 'load_theme_scripts' );
+
+    function load_theme_scripts() {
+        global $wp_scripts; 
+        $wp_scripts->registered[ 'wc-cart' ]->src = get_stylesheet_directory_uri() . '/js/cart.js';
+    }
+
+
 //подключаем скрипт для работы кнопок изменения количества товара
 add_action( 'wp_enqueue_scripts', 'true_include_myscript' );
 function true_include_myscript() {
