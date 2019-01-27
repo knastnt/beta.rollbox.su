0de61bb5d69aa0cbd931a6a900435450e5741c5a
knastnt <456kot@mail.ru>
2019-01-27 21:33:47 +1000

    нашел где добавляемся сообщение что карта обновлена

--- wp-content/plugins/woocommerce/includes/class-wc-form-handler.php
+++ wp-content/plugins/woocommerce/includes/class-wc-form-handler.php
@@ -633,7 +633,8 @@ class WC_Form_Handler {
 				wp_safe_redirect( wc_get_checkout_url() );
 				exit;
 			} elseif ( $cart_updated ) {
-				wc_add_notice( __( 'Cart updated.', 'woocommerce' ) );
+				//Здесь добавляется сообщение Корзина обновлена.
+			    wc_add_notice( __( 'Cart updated.', 'woocommerce' ) );
 				$referer = remove_query_arg( array( 'remove_coupon', 'add-to-cart' ), ( wp_get_referer() ? wp_get_referer() : wc_get_cart_url() ) );
 				wp_safe_redirect( $referer );
 				exit;
