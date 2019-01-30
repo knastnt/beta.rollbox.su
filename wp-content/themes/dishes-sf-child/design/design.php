a063c5af471b28f08cb8d992cef3a094d1a678e4
knastnt <456kot@mail.ru>
2019-01-30 11:33:25 +1000

    вот это уже другое дело. шикарно

--- wp-content/themes/dishes-sf-child/design/design.php
+++ wp-content/themes/dishes-sf-child/design/design.php
@@ -1,32 +1,22 @@
 <?php
 
 
-//Нужно подвинуть этот экшн ниже чтобы вставить перед ним header_top. Меняем ему порядок
-add_action( 'storefront_header', 'replace_storefront_header_container', 1 );
-function replace_storefront_header_container()
-{
-    remove_action('storefront_header', 'storefront_header_container', 0);
-    add_action( 'storefront_header', 'storefront_header_container', 3 );
-}
-
-
-
-add_action( 'storefront_header', 'header_top', 2 );
+//Нужно подвинуть этот экшн перед ним storefront_header_container с нулевым приоритетом. это оказалось просто: приоритет -1
+add_action( 'storefront_header', 'header_top', -1 );
 function header_top() {
     ?>
 
     <div class="header-top">
-        
-        <div class="top-order">
-            <p class="order1">Ordered before 17:30, shipped today  - Support: (012) 800 456 789</p>
-            <p class="order2">Track Your Order</p>
+        <div class="col-full">
+
+            <div class="top-order">
+                <p class="order1">Ordered before 17:30, shipped today  - Support: (012) 800 456 789</p>
+                <p class="order2">Track Your Order</p>
+            </div>
+
         </div>
-
-
-        <hr>
     </div>
 
     <?php
 }
 
-?>
