b04dbbefb85182a9e20a2c42b0459269c18b1b05
knastnt <456kot@mail.ru>
2019-02-04 17:43:37 +1000

    попытка - не пытка

--- wp-content/themes/dishes-sf-child/design/header-cart/header-cart.php
+++ wp-content/themes/dishes-sf-child/design/header-cart/header-cart.php
@@ -38,11 +38,44 @@ if ( ! function_exists( 'storefront_cart_link' ) ) {
 
         <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/design/header-cart/header-cart.css" />
 
-        <button type="button" data-toggle="dropdown" data-loading-text="Loading..."
-                class="btn dropdown-toggle dropdown-cart">
-            <i class="ion-android-cart"></i>
-            <span id="cart-total"><span class="number-cart">0</span>$0.00<i class="ion-chevron-down"></i></span>
-        </button>
+
+
+        <div id="cart" class="btn-group btn-block">
+
+            <button type="button" data-toggle="dropdown" data-loading-text="Loading..."
+                    class="btn dropdown-toggle dropdown-cart">
+                <i class="ion-android-cart"></i>
+                <span id="cart-total"><span class="number-cart">0</span>$0.00<i class="ion-chevron-down"></i></span>
+            </button>
+
+            <ul class="dropdown-menu pull-right" style="display: block;"><li class="has-scroll">
+                    <table class="table">
+                        <tbody><tr>
+                            <td class="text-center cart-image"> <a href="http://demo.towerthemes.com/tt_boxstore/index.php?route=product/product&amp;product_id=30"><img class="cart-image" src="http://demo.towerthemes.com/tt_boxstore/image/cache/catalog/product1/2-100x100.jpg" alt="Aliquam Consequat Eget Non Arc..." title="Aliquam Consequat Eget Non Arc..."></a> </td>
+                            <td class="text-left info-item"><a href="http://demo.towerthemes.com/tt_boxstore/index.php?route=product/product&amp;product_id=30">Aliquam Consequat Eget Non Arc...</a>              <br>
+                                - <small>Select Blue</small>                         			<p class="cart-quantity">Qty:2</p>
+                                <p class="cart-price">$240.00</p>
+                            </td>
+                            <td class="text-center cart-close"><button type="button" onclick="cart.remove('0');" title="Remove" class="btn btn-danger btn-xs"><i class="ion-close-round"></i></button></td>
+                        </tr>
+                        </tbody></table>
+                </li><li>
+                    <table class="table cart-totals">
+                        <tbody><tr>
+                            <td class="text-left">Sub-Total :</td>
+                            <td class="text-right">$240.00</td>
+                        </tr>
+                        <tr>
+                            <td class="text-left">Total :</td>
+                            <td class="text-right">$240.00</td>
+                        </tr>
+                        </tbody></table>
+                    <p class="text-center cart-button">
+                        <a class="view-cart" href="http://demo.towerthemes.com/tt_boxstore/index.php?route=checkout/cart">View Cart</a>
+                        <a class="checkout-cart" href="http://demo.towerthemes.com/tt_boxstore/index.php?route=checkout/checkout">Checkout</a>
+                    </p>
+                </li></ul>
+        </div>
         <?php
     }
 
