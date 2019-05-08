1236e1fa27d6775517a56129edea4dc6410e4451
knastnt <456kot@mail.ru>
2019-05-08 14:36:24 +1000

    комментарий

--- wp-content/themes/dishes-sf-child/functions-modules/woocommerce_ajax_change_quantity/woocommerce_ajax_change_quantity.php
+++ wp-content/themes/dishes-sf-child/functions-modules/woocommerce_ajax_change_quantity/woocommerce_ajax_change_quantity.php
@@ -16,6 +16,7 @@ add_action( 'wp_enqueue_scripts', 'load_theme_scripts' );
 
 
 //подключаем скрипт для работы кнопок изменения количества товара
+//для отображения кнопок нужно подменить файл yourtheme/woocommerce/global/quantity-input.php в своей теме
 add_action( 'wp_enqueue_scripts', 'true_include_myscript' );
 function true_include_myscript() {
  	wp_enqueue_script( 'dishes-sf-child-func', get_stylesheet_directory_uri() . '/functions-modules/woocommerce_ajax_change_quantity/js/dishes-sf-child-func.js' );
