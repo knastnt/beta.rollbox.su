6b7065032d9b3dc084e0dfa362923d4ea2031e87
knastnt <456kot@mail.ru>
2019-05-18 16:28:25 +1000

    лейбл новинка. вывести в настройки сколько дней дудет держаться новинка

--- wp-content/themes/dishes-sf-child/design/productlables/productlables.php
+++ wp-content/themes/dishes-sf-child/design/productlables/productlables.php
@@ -3,7 +3,7 @@
  * кастомные надписи к продуктам.
  *
  */
-// в списке товаров
+/*// в списке товаров
 add_action( 'woocommerce_before_shop_loop_item_title', 'productlables_show_product_loop_lables', 11 );
 // на странице товара
 add_action( 'woocommerce_before_single_product_summary', 'productlables_show_product_lables', 11 );
@@ -13,7 +13,7 @@ if ( ! function_exists( 'productlables_show_product_loop_lables' ) ) {
 
     /**
      * Get the flash for the loop.
-     */
+     *
     function productlables_show_product_loop_lables() {
         template();
     }
@@ -24,7 +24,7 @@ if ( ! function_exists( 'productlables_show_product_lables' ) ) {
 
     /**
      * Output the product flash.
-     */
+     *
     function productlables_show_product_lables() {
         template();
     }
@@ -35,11 +35,35 @@ function template()  {
 
     global $post, $product;
 
+    $created = $product->get_date_created();
+    $now = new DateTime("now");
+    $diff_days = date_diff($now, $created)->days;
+    if ($diff_days < 14) {
 
-    if ( $product->is_on_sale() ) {
-
-        echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $post, $product );
+        echo apply_filters( 'woocommerce_sale_flash', '<span class="new">Новинка!</span>', $post, $product );
 
     }
 
+}*/
+
+add_filter( 'woocommerce_sale_flash', 'custom_labels', 10, 3 );
+
+function custom_labels( $result, $post, $product ) {
+
+    //У нас уже готов лэйбл распродажи. Если существует. Сохраним его в переменную
+    $onsale = $result;
+
+    //Определяем новый ли товар
+    $isnew = '';
+    $created = $product->get_date_created();
+    $now = new DateTime("now");
+    $diff_days = date_diff($now, $created)->days;
+    if ($diff_days < 14) {
+        $isnew = '<span class="new onsale">Новинка!</span>';
+    }
+
+    //Оборачиваем в блок div
+    $result = '<div class="labels">' . $isnew . $onsale . '</div>';
+
+    return $result;
 }
