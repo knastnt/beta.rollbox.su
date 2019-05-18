a92afb8857e3ed9155485012e93b0647a9b78ff7
knastnt <456kot@mail.ru>
2019-05-18 20:56:31 +1000

    новинка и распродажа теперь работают всегда, а не только вместе

--- wp-content/themes/dishes-sf-child/design/productlables/productlables.php
+++ wp-content/themes/dishes-sf-child/design/productlables/productlables.php
@@ -3,55 +3,28 @@
  * кастомные надписи к продуктам.
  *
  */
-/*// в списке товаров
-add_action( 'woocommerce_before_shop_loop_item_title', 'productlables_show_product_loop_lables', 11 );
+
+
+//// Убираем стандартный лэйбл (т.к. его нужно обернуть в общий div, а так не получается).
+add_filter( 'woocommerce_sale_flash', 'remove_default_onsale', 10, 1 );
+function remove_default_onsale( $result ) {
+    return '';
+}
+
+///// Добавляем кастомные лэйблы. В том числе и стандартный лэйбл Распродажа
+// в списке товаров
+add_action( 'woocommerce_before_shop_loop_item_title', 'add_custom_labels', 11 );
 // на странице товара
-add_action( 'woocommerce_before_single_product_summary', 'productlables_show_product_lables', 11 );
-
-
-if ( ! function_exists( 'productlables_show_product_loop_lables' ) ) {
-
-    /**
-     * Get the flash for the loop.
-     *
-    function productlables_show_product_loop_lables() {
-        template();
-    }
-}
-
-
-if ( ! function_exists( 'productlables_show_product_lables' ) ) {
-
-    /**
-     * Output the product flash.
-     *
-    function productlables_show_product_lables() {
-        template();
-    }
-}
-
-
-function template()  {
+add_action( 'woocommerce_before_single_product_summary', 'add_custom_labels', 11 );
+function add_custom_labels() {
 
     global $post, $product;
 
-    $created = $product->get_date_created();
-    $now = new DateTime("now");
-    $diff_days = date_diff($now, $created)->days;
-    if ($diff_days < 14) {
-
-        echo apply_filters( 'woocommerce_sale_flash', '<span class="new">Новинка!</span>', $post, $product );
-
+    //Определяем есть ли скидка у товара
+    $onsale = '';
+    if ( $product->is_on_sale() ){
+        $onsale = '<div class="onsale">Распродажа!</div>';
     }
-
-}*/
-
-add_filter( 'woocommerce_sale_flash', 'custom_labels', 10, 3 );
-
-function custom_labels( $result, $post, $product ) {
-
-    //У нас уже готов лэйбл распродажи. Если существует. Сохраним его в переменную
-    $onsale = $result;
 
     //Определяем новый ли товар
     $isnew = '';
@@ -59,11 +32,11 @@ function custom_labels( $result, $post, $product ) {
     $now = new DateTime("now");
     $diff_days = date_diff($now, $created)->days;
     if ($diff_days < 14) {
-        $isnew = '<span class="new onsale">Новинка!</span>';
+        $isnew = '<div class="new onsale">Новинка!</div>';
     }
 
     //Оборачиваем в блок div
     $result = '<div class="labels">' . $isnew . $onsale . '</div>';
 
-    return $result;
+    echo $result;
 }
