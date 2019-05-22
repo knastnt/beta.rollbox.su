a2e76d6292c3ebecda56097616de7d8b2b1188c7
knastnt <456kot@mail.ru>
2019-05-22 16:16:41 +1000

    скрытие блока новинок, если их нет

--- wp-content/themes/dishes-sf-child/homepage/first-sidebar.php
+++ wp-content/themes/dishes-sf-child/homepage/first-sidebar.php
@@ -5,22 +5,7 @@
  * Date: 22.05.2019
  * Time: 16:05
  */
-?>
 
-<div class="products-slick-slider num2">
-    <div class="title">
-        <h4>
-            Новинки
-        </h4>
-        <div class="nav">
-            <div class="nav-prev">
-                <i class="arrow-back"></i>
-            </div><div class="nav-break"></div><div class="nav-next">
-                <i class="arrow-forward"></i>
-            </div>
-        </div>
-    </div>
-    <?php
 
 
     // Выводим новинки
@@ -62,39 +47,61 @@
     $result = str_replace('</ul', '</div', $result);
     $result = str_replace('<li', '<div', $result);
     $result = str_replace('</li', '</div', $result);
-    echo $result;
 
-    ?>
+    if (strlen($result) > 50){
 
-    <script type="text/javascript">
-        (function() {
-            jQuery('.center-section .first .sidebar .num2 .products').slick({
-                swipeToSlide: true,
-                infinite: true,
-                dots: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                responsive: [
-                    {
-                        breakpoint: 1064,
-                        settings: {
-                            slidesToShow: 1
-                        }
-                    },
-                    {
-                        breakpoint: 767,
-                        settings: {
-                            slidesToShow: 1
-                        }
-                    }
-                ],
-                arrows: true,
-                nextArrow: jQuery('.products-slick-slider.num2 .nav-next'),
-                prevArrow: jQuery('.products-slick-slider.num2 .nav-prev')
-            });
-        })()
-    </script>
-</div>
+        ?>
+        <div class="products-slick-slider num2">
+            <div class="title">
+                <h4>
+                Новинки
+                </h4>
+                <div class="nav">
+                    <div class="nav-prev">
+                        <i class="arrow-back"></i>
+                    </div><div class="nav-break"></div><div class="nav-next">
+                        <i class="arrow-forward"></i>
+                    </div>
+                </div>
+            </div>
+
+            <?php echo $result; ?>
+
+            <script type="text/javascript">
+                (function() {
+                    jQuery('.center-section .first .sidebar .num2 .products').slick({
+                        swipeToSlide: true,
+                        infinite: true,
+                        dots: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        responsive: [
+                            {
+                                breakpoint: 1064,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            },
+                            {
+                                breakpoint: 767,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            }
+                        ],
+                        arrows: true,
+                        nextArrow: jQuery('.products-slick-slider.num2 .nav-next'),
+                        prevArrow: jQuery('.products-slick-slider.num2 .nav-prev')
+                    });
+                })()
+            </script>
+        </div>
+        <?php
+    }
+
+?>
+
+
 <div class="products-slick-slider num3">
     <div class="title">
         <h4>
