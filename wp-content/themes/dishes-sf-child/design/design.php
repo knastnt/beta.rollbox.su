2bbcf34386f71d6320da2e87eca491f263c2e3ea
knastnt <456kot@mail.ru>
2019-05-28 15:49:22 +1000

    common.js теперь подключается в футере

--- wp-content/themes/dishes-sf-child/design/design.php
+++ wp-content/themes/dishes-sf-child/design/design.php
@@ -15,6 +15,7 @@ require_once( get_stylesheet_directory() . '/design/stickeymenu/stickeymenu.php'
 
 
 //подключаем js с общими функциями
-add_action( 'wp_enqueue_scripts', function (){
-    wp_enqueue_script( 'commonscript', get_stylesheet_directory_uri() . '/design/common.js');
-});
+add_action( 'wp_enqueue_scripts', 'true_include_commonscript' );
+function true_include_commonscript() {
+    wp_enqueue_script( 'commonscript', get_stylesheet_directory_uri() . '/design/common.js', array('jquery'), null, true );
+}
