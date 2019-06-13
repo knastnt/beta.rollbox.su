1694c53313c00b19f2438e94c63c95da11a472dc
knastnt <456kot@mail.ru>
2019-06-13 13:07:29 +1000

    пишу функцию начисления бонусов за заказы add_bonus_when_order_complete

--- wp-content/plugins/woocommerce-loyalty/coupons/coupons.php
+++ wp-content/plugins/woocommerce-loyalty/coupons/coupons.php
@@ -13,6 +13,9 @@ add_action( 'woocommerce_coupon_options_save', array('Coupons', 'save_conly_for_
 //Проверка значения поня при применении купона
 add_action( 'woocommerce_coupon_is_valid', array('Coupons', 'check_user_id_when_coupon_apply'), 10, 3);
 
+//Начисление бонусов при выполнении заказа
+add_action( 'woocommerce_order_status_completed', array( 'Coupons', 'add_bonus_when_order_complete' ) );
+
 class Coupons
 {
 
@@ -41,5 +44,43 @@ class Coupons
         }
 
         return $true_p;
+    }
+
+    //Начисление бонусов при выполнении заказа
+    static function add_bonus_when_order_complete( $order_ID ) {
+
+        //Получаем ШВ пользователя
+        $user_id = get_current_user_id();
+        // Если юзер не залогинен, то ничего не делаем
+        if ($user_id == 0) return;
+
+        //Получаем значение процента возврата от суммы ордера
+        $percentOfPointReturning = woocommerceLoyalty_Options::instance()->getPercentOfPointReturning();
+        //Если 0, то ничего не делаем
+        if ($percentOfPointReturning == 0) return;
+
+        //Получаем ордер
+        $order = wc_get_order( $order_ID );
+        //Если такого ордера нет, то ничего не делаем
+        if (!$order) return;
+
+        //Получаем количество бонусов для возврата
+        $pointsToReturn = intval($order->get_total() * $percentOfPointReturning * 0.01);
+        //Если 0, то ничего не делаем
+        if ($pointsToReturn == 0) return;
+
+
+        //Получаем метаданные пользователя, относящиеся к плагину
+        $wc_loy_usermeta = new WC_Loy_UserMeta($user_id);
+
+        //Проверка, начислялись ли бонусы за этот заказ
+        $origincode = 'fromOrder_' . $order_ID; //Генерируем значение источника и проверяем нет ли такого у пользователя
+
+
+
+
+        $wc_loy_usermeta->addPoints();
+
+        echo $order_ID;
     }
 }
