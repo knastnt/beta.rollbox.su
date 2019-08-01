ba09be1f35e49beb7743a69a949546a9e860f229
knastnt <456kot@mail.ru>
2019-08-02 09:49:58 +1000

    Исправил баг с начисление за отзывы. Теперь начисляются нормально при аппруве и без него. а также при аппруве ответом

--- wp-content/plugins/woocommerce-loyalty/coupons/coupons.php
+++ wp-content/plugins/woocommerce-loyalty/coupons/coupons.php
@@ -17,8 +17,10 @@ add_action( 'woocommerce_coupon_is_valid', array('Coupons', 'check_user_id_when_
 add_action( 'woocommerce_order_status_completed', array( 'Coupons', 'add_bonus_when_order_complete' ) );
 //Начисление бонусов за регистрацию
 add_action( 'user_register', array( 'Coupons', 'add_bonus_when_user_registred') );
+
 //Начисление бонусов за оставление комментариев
-add_action( 'comment_post', array( 'Coupons', 'add_comment_to_product'), 10, 2 );
+add_action('comment_post', array( 'Coupons', 'post_comment'), 10, 2);
+add_action('transition_comment_status', array( 'Coupons', 'change_comment_status'), 10, 3);
 
 class Coupons
 {
@@ -115,43 +117,59 @@ class Coupons
     }
 
 
-    //Начисление бонусов за оставление комментариев
-    static function add_comment_to_product( $comment_ID, $comment_approved ) {
-
+    /**
+     * Если комментарий требует одобрения, то он проходит через изменение статуса, если не требует - то через post_comment
+     */
+    //Начисление бонусов за оставление комментариев при изменении статуса на approved
+    static function change_comment_status( $new_status, $old_status, $wp_comment ) {
+       if( $new_status == 'approved' ){
+           self::add_bonus_for_comment( $wp_comment );
+       }
+    }
+    //Начисление бонусов за оставление комментариев при публикации
+    static function post_comment( $comment_ID, $comment_approved ) {
         //Получаем комментарий из его ID
         $wp_comment = get_comment( $comment_ID );
         // Если коммент не существует, то ничего не делаем
         if ($wp_comment == null) return;
+
+        if( 1 === $comment_approved ){
+            self::add_bonus_for_comment( $wp_comment );
+        }
+    }
+
+    //Начисление бонусов за комментарий
+    static function add_bonus_for_comment( $wp_comment ) {
 
         //Получаем ID пользователя, оставившего коммент
         $user_id = $wp_comment->user_id;
         // Если юзер не существует, то ничего не делаем
         if ($user_id == 0) return;
 
-        if( 1 === $comment_approved ){
-            //function logic goes here
-            $post_ID = $wp_comment->comment_post_ID; //ID записи, на которой оставили этот коммент
-            $post = get_post($post_ID);
-            if ($post->post_type == "product") {
-                //Если коммент оставлен товару
 
-                //Получаем количество бонусов для зачисления за отзыв
-                $pointsForReview = woocommerceLoyalty_Options::instance()->getPointsForReview();
-                //Если 0, то ничего не делаем
-                if ($pointsForReview == 0) return;
+        //ID записи, на которой оставили этот коммент
+        $post_ID = $wp_comment->comment_post_ID;
+        $post = get_post($post_ID);
+        if ($post->post_type == "product") {
+            //Если коммент оставлен товару
 
-                //Получаем метаданные пользователя, относящиеся к плагину
-                $wc_loy_usermeta = new WC_Loy_UserMeta($user_id);
+            //Получаем количество бонусов для зачисления за отзыв
+            $pointsForReview = woocommerceLoyalty_Options::instance()->getPointsForReview();
+            //Если 0, то ничего не делаем
+            if ($pointsForReview == 0) return;
 
-                //Проверка, начислялись ли бонусы за комментирование этого товара
-                $origincode = 'fromProductComment_' . $post_ID; //Генерируем значение источника и проверяем нет ли такого у пользователя
-                $isOriginCodeExistInHistory = $wc_loy_usermeta->isOriginCodeExistInHistory($origincode);
-                //Если бонусы за это уже начислялись - то ничего не делаем
-                if ($isOriginCodeExistInHistory == true) return;
+            //Получаем метаданные пользователя, относящиеся к плагину
+            $wc_loy_usermeta = new WC_Loy_UserMeta($user_id);
 
-                //Если все тесты пройдены - начисляем бонусы
-                $wc_loy_usermeta->addPoints($pointsForReview, 'Начисление за отзыв. <a href="' . get_permalink( $post_ID ) . '">' . $post->post_title . '</a>', $origincode);
-            }
+            //Проверка, начислялись ли бонусы за комментирование этого товара
+            $origincode = 'fromProductComment_' . $post_ID; //Генерируем значение источника и проверяем нет ли такого у пользователя
+            $isOriginCodeExistInHistory = $wc_loy_usermeta->isOriginCodeExistInHistory($origincode);
+            //Если бонусы за это уже начислялись - то ничего не делаем
+            if ($isOriginCodeExistInHistory == true) return;
+
+            //Если все тесты пройдены - начисляем бонусы
+            $wc_loy_usermeta->addPoints($pointsForReview, 'Начисление за отзыв. <a href="' . get_permalink( $post_ID ) . '">' . $post->post_title . '</a>', $origincode);
         }
+
     }
 }
