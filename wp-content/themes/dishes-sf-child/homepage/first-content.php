1117f27de9ad3963f01c65dd22991a847283392c
knastnt <456kot@mail.ru>
2019-08-06 15:59:34 +1000

    собрал популярное, новинки и со скидками в одну кучу

--- wp-content/themes/dishes-sf-child/homepage/first-content.php
+++ wp-content/themes/dishes-sf-child/homepage/first-content.php
@@ -11,6 +11,83 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/design/slick/slick.css"/>
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/design/slick/slick-theme.css"/>
 
+
+<?php
+
+    //Популятные товары
+    $popular_content = do_shortcode( '[best_selling_products per_page="12"]' );
+    $popular_content = str_replace('<ul', '<div', $popular_content);
+    $popular_content = str_replace('</ul', '</div', $popular_content);
+    $popular_content = str_replace('<li', '<div', $popular_content);
+    $popular_content = str_replace('</li', '</div', $popular_content);
+
+
+    // Блок Товары со скидкой
+    $isSalesExist = false;
+    $salesContent = '';
+    $shortcode = new WC_Shortcode_Products( array(),'sale_products' );
+    if ( isset($shortcode->get_query_args()['post__in']) && count($shortcode->get_query_args()['post__in']) > 1 ) {
+        $isSalesExist = true;
+
+        $salesContent = $shortcode->get_content();
+        $salesContent = str_replace('<ul', '<div', $salesContent);
+        $salesContent = str_replace('</ul', '</div', $salesContent);
+        $salesContent = str_replace('<li', '<div', $salesContent);
+        $salesContent = str_replace('</li', '</div', $salesContent);
+    }
+
+
+    // Новинки
+    $isNewExist = false;
+    $newContent = '';
+    //////////////////////////////////////////////////////////////
+
+    //Узнаем сколько дней товар считается новым
+    $new_product_duration = isset(get_option( 'rollbox_options_array' ) ['new_product_duration']) ? get_option( 'rollbox_options_array' ) ['new_product_duration'] : 14;
+
+    $args = array(
+        'post_type' => 'product', // тип товара
+        'date_query' => array(
+            array(
+                'after'  => $new_product_duration . ' day ago',
+            )
+        )
+    );
+
+    $loop = new WP_Query( $args );
+
+    ob_start();
+
+    woocommerce_product_loop_start();
+
+    while ( $loop->have_posts() ) {
+        $loop->the_post();
+        wc_get_template_part( 'content', 'product' );
+    }
+
+    woocommerce_product_loop_end();
+
+    $result = ob_get_clean();
+    //////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////
+    //$result = do_shortcode( '[recent_products per_page="12"]' );
+    //////////////////////////////////////////////////////////////
+
+    $result = str_replace('<ul', '<div', $result);
+    $result = str_replace('</ul', '</div', $result);
+    $result = str_replace('<li', '<div', $result);
+    $result = str_replace('</li', '</div', $result);
+
+    if (strlen($result) > 50){
+        $isNewExist = true;
+        $newContent = $result;
+    }
+
+?>
+
+
+
 <div class="products-slick-slider num1">
     <div class="title">
         <div class="tab-wrapper">
@@ -35,12 +112,12 @@
     </div>
 
     <div class="content-wrapper">
-        <input type="radio" name="odin" checked="checked" id="vkl1"/>
-        <label for="vkl1">CSS</label>
-        <input type="radio" name="odin" id="vkl2"/>
-        <label for="vkl2">HTML</label>
-        <input type="radio" name="odin" id="vkl3"/>
-        <label for="vkl3">Дополнительно</label>
+        <input type="radio" name="odin" checked="checked" id="popular"/>
+        <label for="popular">Самые популярные</label>
+        <input type="radio" name="odin" id="sale"/>
+        <label for="sale">Со скидками</label>
+        <input type="radio" name="odin" id="new"/>
+        <label for="new">Новинки</label>
         <style>
             .content-wrapper > div, .content-wrapper > input { display: none; }
 
@@ -52,50 +129,70 @@
             .content-wrapper > input:nth-of-type(3):checked ~ div:nth-of-type(3) { display: block; padding: 5px; border: 1px solid #aaa; }
         </style>
 
+        <script type="text/javascript">
+
+            (function() {
+                jQuery('.content-wrapper input').on('change', function() {
+
+                    try {
+                        jQuery('#' + this.id + ' .products').slick('refresh');
+                    }catch (e) {
+                        jQuery('#' + this.id + ' .products').slick({
+                            swipeToSlide: true,
+                            infinite: false,
+                            dots: true,
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                            responsive: [
+                                {
+                                    breakpoint: 1064,
+                                    settings: {
+                                        slidesToShow: 3
+                                    }
+                                },
+                                {
+                                    breakpoint: 767,
+                                    settings: {
+                                        slidesToShow: 1
+                                    }
+                                }
+                            ],
+                            arrows: true,
+                            nextArrow: jQuery('.products-slick-slider.num1 .nav-next'),
+                            prevArrow: jQuery('.products-slick-slider.num1 .nav-prev')
+                        });
+                    }
+
+
+                });
+
+
+            })()
+        </script>
+
         <div id="popular" class="content-entry">
             <?php
-            $result = do_shortcode( '[best_selling_products per_page="12"]' );
-            $result = str_replace('<ul', '<div', $result);
-            $result = str_replace('</ul', '</div', $result);
-            $result = str_replace('<li', '<div', $result);
-            $result = str_replace('</li', '</div', $result);
-            echo $result;
+
+            echo $popular_content;
             ?>
 
-            <script type="text/javascript">
-                (function() {
-                    jQuery('.center-section .first .content .num1 .woocommerce .products').slick({
-                        swipeToSlide: true,
-                        infinite: false,
-                        dots: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 1,
-                        responsive: [
-                            {
-                                breakpoint: 1064,
-                                settings: {
-                                    slidesToShow: 3
-                                }
-                            },
-                            {
-                                breakpoint: 767,
-                                settings: {
-                                    slidesToShow: 1
-                                }
-                            }
-                        ],
-                        arrows: true,
-                        nextArrow: jQuery('.products-slick-slider.num1 .nav-next'),
-                        prevArrow: jQuery('.products-slick-slider.num1 .nav-prev')
-                    });
-                })()
-            </script>
-        </div>
-        <div id="sales" class="content-entry">
 
+        </div>
+        <div id="sale" class="content-entry">
+            <?php
+                // Блок Товары со скидкой
+                if ( $isSalesExist ) {
+                     echo $salesContent;
+                }
+            ?>
         </div>
         <div id="new" class="content-entry">
-
+            <?php
+                // Блок Новинки
+                if ( $isNewExist ) {
+                    echo $newContent;
+                }
+            ?>
         </div>
     </div>
 </div>
@@ -205,69 +302,6 @@
     <!--img src="http://demo.towerthemes.com/tt_boxstore/image/cache/catalog/category/img1-category-870x125.jpg" alt=""-->
     <?php print_homepage_map(); ?>
 </div>
-
-<?php
-// Блок Товары со скидкой
-$shortcode = new WC_Shortcode_Products( array(),'sale_products' );
-if ( isset($shortcode->get_query_args()['post__in']) && count($shortcode->get_query_args()['post__in']) > 1 ) {
-    ?>
-
-    <div class="products-slick-slider num5">
-        <div class="title">
-            <h4>
-                Товары со скидкой
-            </h4>
-            <div class="nav">
-                <div class="nav-prev">
-                    <i class="arrow-back"></i>
-                </div>
-                <div class="nav-break"></div>
-                <div class="nav-next">
-                    <i class="arrow-forward"></i>
-                </div>
-            </div>
-        </div>
-        <?php
-        $result = $shortcode->get_content();
-        $result = str_replace('<ul', '<div', $result);
-        $result = str_replace('</ul', '</div', $result);
-        $result = str_replace('<li', '<div', $result);
-        $result = str_replace('</li', '</div', $result);
-        echo $result;
-        ?>
-
-        <script type="text/javascript">
-            (function () {
-                jQuery('.center-section .first .content .num5 .woocommerce .products').slick({
-                    swipeToSlide: true,
-                    infinite: false,
-                    dots: true,
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                    responsive: [
-                        {
-                            breakpoint: 1064,
-                            settings: {
-                                slidesToShow: 3
-                            }
-                        },
-                        {
-                            breakpoint: 767,
-                            settings: {
-                                slidesToShow: 1
-                            }
-                        }
-                    ],
-                    arrows: true,
-                    nextArrow: jQuery('.products-slick-slider.num5 .nav-next'),
-                    prevArrow: jQuery('.products-slick-slider.num5 .nav-prev')
-                });
-            })()
-        </script>
-    </div>
-    <?php
-}
-?>
 
 
 
