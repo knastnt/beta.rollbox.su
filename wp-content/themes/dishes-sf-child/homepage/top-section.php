095edb1ac2dad3200eebe45cb07c20f5c242e699
knastnt <456kot@mail.ru>
2019-09-01 18:32:55 +1000

    2 версия баннера для мобильной версии

--- wp-content/themes/dishes-sf-child/homepage/top-section.php
+++ wp-content/themes/dishes-sf-child/homepage/top-section.php
@@ -20,7 +20,8 @@
             <img src="<?php echo get_stylesheet_directory_uri() . '/homepage/nivoslider/img/' ?>banner_kalyan_ps_315_405.jpg" alt="" />
         </div>
         <div class="banner2">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/homepage/nivoslider/img/' ?>banner_mini_orderrolls_315_138.jpg" alt="" />
+            <img class="desktop" src="<?php echo get_stylesheet_directory_uri() . '/homepage/nivoslider/img/' ?>banner_mini_orderrolls_315_138.jpg" alt="" />
+            <img class="mobile" src="<?php echo get_stylesheet_directory_uri() . '/homepage/nivoslider/img/' ?>banner_mini_orderrolls_315_405.jpg" alt="" />
         </div>
     </div>
     <div style="clear: both"></div>
