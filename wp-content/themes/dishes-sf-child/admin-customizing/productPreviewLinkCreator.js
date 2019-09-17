a62f977eacd1c121f2bbc7ff301291c0803f0151
knastnt <456kot@mail.ru>
2019-09-17 10:37:21 +1000

    получаем slug

--- wp-content/themes/dishes-sf-child/admin-customizing/productPreviewLinkCreator.js
+++ wp-content/themes/dishes-sf-child/admin-customizing/productPreviewLinkCreator.js
@@ -1,6 +1,6 @@
 jQuery(document).on('click', '.post-type-shop_order .wp-list-table tbody td').on('click', '.order-preview:not(.disabled)', function () {
     //Вызывается при нажатии на глаз
-    addUrlToProducts(3);
+    addUrlToProducts(30); //30 секунд будем ожидать появления окна, чтобы добавить ссылок
 });
 
 
@@ -8,11 +8,41 @@ function addUrlToProducts(i) {
     if (i < 0) return;
 
     setTimeout(function () {
+        var modalDialog = jQuery('#wc-backbone-modal-dialog');
+        if (modalDialog.length == 0) {
+            //Если окна нет, то ждем дальше
+            addUrlToProducts(--i);
+            return;
+        };
+        //Далее уже при присутствующем окне
 
-        alert("Hello " + i);
+        //var products = jQuery(modalDialog).find('tr.wc-order-preview-table__item');
 
-        addUrlToProducts(--i);
+        //[class ^= pre]
 
-    }, 2000);
+        jQuery(modalDialog).find('tr.wc-order-preview-table__item').each(function(i,elem) {
+            var classes = jQuery(this).attr('class').split(/\s+/);
+
+            jQuery.each(classes,function(index,value){
+                if (value.indexOf("slug--") == 0) {
+                    var slug = value.substr(6);
+                    console.log(slug);
+                }
+            });
+            //alert(jQuery(this).attr('class'));
+            /*if ($(this).hasClass("stop")) {
+                alert("Остановлено на " + i + "-м пункте списка.");
+                return false;
+            } else {
+                alert(i + ': ' + $(elem).text());
+            }*/
+        });
+
+        alert(modalDialog.length + " " + i);
+
+
+
+
+    }, 1000); //задержка перед выполнением 1 секунда
 }
 
